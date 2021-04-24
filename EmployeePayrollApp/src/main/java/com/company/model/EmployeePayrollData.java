537f8f38949528baee5278cd8cbb1088a9d190dc
Bhushan Patil <bhupatil001@gmail.com>
2021-04-24 16:29:31 +0530

    Added: use lombok library to auto generate getters and setters method delclaring @Data anotation class level

--- EmployeePayrollApp/src/main/java/com/company/model/EmployeePayrollData.java
+++ EmployeePayrollApp/src/main/java/com/company/model/EmployeePayrollData.java
@@ -2,6 +2,8 @@ package com.company.model;
 
 import com.company.dto.EmployeePayrollDTO;
 
+import lombok.Data;
+@Data
 public class EmployeePayrollData {
 
 	private int employeeId;
@@ -24,30 +26,6 @@ public class EmployeePayrollData {
 		this.employeeId = employeeId;
 		this.name = employeePayrollDTO.name;
 		this.salary = employeePayrollDTO.salary;
-	}
-
-	public int getEmployeeId() {
-		return employeeId;
-	}
-
-	public void setEmployeeId(int employeeId) {
-		this.employeeId = employeeId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public long getSalary() {
-		return salary;
-	}
-
-	public void setSalary(long salary) {
-		this.salary = salary;
 	}
 
 }
--- EmployeePayrollApp/src/main/java/com/company/service/EmployeePayrollService.java
+++ EmployeePayrollApp/src/main/java/com/company/service/EmployeePayrollService.java
@@ -12,17 +12,20 @@ import com.company.model.EmployeePayrollData;
 public class EmployeePayrollService implements IEmployeePayrollService {
 
 	private List<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-
+	
+	//get all
 	@Override
 	public List<EmployeePayrollData> getEmployeePayrollData() {
 		return employeePayrollList;
 	}
-
+	
+	//get by id
 	@Override
 	public EmployeePayrollData getEmployeePayrollDataById(int empId) {
 		return employeePayrollList.get(empId - 1);
 	}
-
+	
+	//create
 	@Override
 	public EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData payrollData = null;
@@ -30,16 +33,18 @@ public class EmployeePayrollService implements IEmployeePayrollService {
 		employeePayrollList.add(payrollData);
 		return payrollData;
 	}
-
+	
+	//update
 	@Override
 	public EmployeePayrollData updateEmployeePayrollData(int empId, EmployeePayrollDTO employeePayrollDTO) {
 		EmployeePayrollData payrollData = this.getEmployeePayrollDataById(empId);
 		payrollData.setName(employeePayrollDTO.name);
 		payrollData.setSalary(employeePayrollDTO.salary);
-		employeePayrollList.set(empId - 1, payrollData);
+ 		employeePayrollList.set(empId - 1, payrollData);
 		return payrollData;
 	}
-
+	
+	//delete
 	@Override
 	public void deleteEmployeePayrollData(int empId) {
 		employeePayrollList.remove(empId - 1);
