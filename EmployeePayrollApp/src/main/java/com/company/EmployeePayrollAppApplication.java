54b392386be9d90e1572b66dc7dac52e65ad175d
Bhushan Patil <bhupatil001@gmail.com>
2021-04-24 20:55:53 +0530

    Added: Database setting as environment variable

--- EmployeePayrollApp/src/main/java/com/company/EmployeePayrollAppApplication.java
+++ EmployeePayrollApp/src/main/java/com/company/EmployeePayrollAppApplication.java
@@ -13,6 +13,11 @@ public class EmployeePayrollAppApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollAppApplication.class, args);
 		log.info("Employee Payroll app started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is = {} ",
+				context.getEnvironment().getProperty("spring.datasource.username"));
+		log.info("Employee Payroll DB password is = {} ",
+				context.getEnvironment().getProperty("spring.datasource.password"));
+
 	}
 
 }
